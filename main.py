2d8e79090519964a3f72f7bc188d8e2b5c795c81
VictorHugoRaisCardoso <victornegocio.expert@gmail.com>
2024-11-21 21:18:47 -0300

    1.0.0.2 BETA
    Bug do áudio que não reproduzia foi resolvido, ainda resta otimizar o processamento dos áudios a cada toque para que reduza eventuais delays.
    Próximo passo é melhorar a interface do app e compilar a primeira versão apk do código.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import pygame
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.core.audio import SoundLoader
-from kivy.clock import Clock
+import pygame
 
+pygame.mixer.init()
+pygame.mixer.set_num_channels(20)
 
-pygame.init()
 
 
 class Tela(BoxLayout):
+    sons = {}
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    def tocar_audio(self, arquivo):
-        toque = pygame.mixer.Sound(arquivo)
-        toque.play()
+        self.sons['1.wav'] = pygame.mixer.Sound('1.wav')
+        self.sons['2.wav'] = pygame.mixer.Sound('2.wav')
+        self.sons['3.wav'] = pygame.mixer.Sound('3.wav')
+    
+    def tocar_audio(self, tipo):
+        som = self.sons.get(tipo)
+        if som:
+            som.play()
 
 class Main(App):
     def build(self):
